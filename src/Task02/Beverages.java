7575a24a5242207939c232cc7db54efe4a170c17
Visal Hewage <visalsathsara55@gmail.com>
2024-08-22 23:54:05 +0530

    task 02 full commit

--- src/Task02/Beverages.java
+++ src/Task02/Beverages.java
@@ -1,4 +1,31 @@
 package Task02;
 
-public class Beverages {
+public abstract class Beverages {
+    private boolean wantsExtras;
+
+    public final void finalTemplateMethod() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+        if (wantsExtras) {
+            addExtras();
+        }
+    }
+
+    public void setWantsExtras(boolean wantsExtras) {
+        this.wantsExtras = wantsExtras;
+    }
+
+    public abstract void brew();
+    public abstract void addCondiments();
+    public abstract void addExtras();
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
 }
